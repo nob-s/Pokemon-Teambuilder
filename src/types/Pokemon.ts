7fb08f1327a06b62c1480a8077ec2e78c3edb4fa
nob-s <jqlee999@gmail.com>
2026-03-12 18:37:49 +0800

    WIP stat calculation

--- src/types/Pokemon.ts
+++ src/types/Pokemon.ts
@@ -1,4 +1,5 @@
 import type { Ability } from "./Ability.ts";
+import * as assert from "node:assert";
 
 export class Pokemon {
     private static readonly ERROR_NAME = "Error fetching pokemon info... Try again!";
@@ -8,20 +9,42 @@ export class Pokemon {
     public static readonly EMPTY_POKEMON = Pokemon.emptyPokemonWithName(Pokemon.EMPTY_NAME);
     public static readonly SEARCH_PROMPT_POKEMON = Pokemon.emptyPokemonWithName(Pokemon.SEARCH_PROMPT_NAME);
 
+    private readonly POKEMON_MAX_IV = 31;
+
     name: string;
     id: number;
     types: string[];
-    stats: Record<string, number>;
+    baseStats: Record<string, number>;
     abilities: Ability[];
     spriteUrl: string;
 
-    constructor(name: string, id: number, types: string[], stats: Record<string, number>, abilities: Ability[], spriteUrl: string) {
+    ivs: Record<string, number>;
+
+    constructor(name: string, id: number, types: string[], baseStats: Record<string, number>, abilities: Ability[], spriteUrl: string) {
         this.name = name;
         this.id = id;
-        this.abilities = abilities;
-        this.stats = stats;
         this.types = types;
+        this.baseStats = baseStats;
+        this.abilities = abilities;
         this.spriteUrl = spriteUrl;
+        // Modifiable data
+        this.ivs = this.setMaxIvs(baseStats);
+
+    }
+
+    private setMaxIvs(baseStats: Record<string, number>): Record<string, number> {
+        const maxIvs: Record<string, number> = {};
+        for (const key in Object.entries(baseStats)) {
+            maxIvs[key] = this.POKEMON_MAX_IV;
+        }
+        return maxIvs;
+    }
+
+    private calculateStat(statName: string, base: number, iv: number, level: number, ev: number): number {
+        
+        if (statName.toLowerCase() === "hp") {
+
+        }
     }
 
     private static emptyPokemonWithName(name: string): Pokemon {
